531292e8feb4103083be74d8aa0764b84c8bfd27
sai4638 <160169580+sai4638@users.noreply.github.com>
2025-03-01 17:11:52 -0500

    f

--- R15_SEC02_J.java
+++ R15_SEC02_J.java
@@ -1,9 +1,10 @@
-public RandomAccessFile openFile(final java.io.File f) {
-  askUserPermission(f.getPath());
+public RandomAccessFile openFile(java.io.File f) {
+  final java.io.File copy = new java.io.File(f.getPath());
+  askUserPermission(copy.getPath());
   // ...
   return (RandomAccessFile)AccessController.doPrivileged(new PrivilegedAction <Object>() {
     public Object run() {
-      return new RandomAccessFile(f, f.getPath());
+      return new RandomAccessFile(copy, copy.getPath());
     }
   });
 }

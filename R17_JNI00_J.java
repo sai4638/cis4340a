1f46e8bd1d9ed9c66b973f63d3710b6c55645bdb
sai4638 <160169580+sai4638@users.noreply.github.com>
2025-03-01 17:14:48 -0500

    Create R17_JNI00_J.java

---
+++ R17_JNI00_J.java
@@ -0,0 +1,15 @@
+public final class NativeMethod {
+ 
+  // Public native method
+  public native void nativeOperation(byte[] data, int offset, int len);
+ 
+  // Wrapper method that lacks security checks and input validation
+  public void doOperation(byte[] data, int offset, int len) {
+    nativeOperation(data, offset, len);
+  }
+   
+  static {
+    // Load native library in static initializer of class
+    System.loadLibrary("NativeMethodLib");
+  }
+}
